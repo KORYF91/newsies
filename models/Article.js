a04bba03c36257b82f78f86446f50fe9b06debc7
Kfriedman <Koryf91@gmail.com>
2020-02-23 20:03:59 -0800

    fixed link to data base. using mongoose

--- models/Article.js
+++ models/Article.js
@@ -6,32 +6,36 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
   link: {
     type: String,
-    required: true
+    required: true 
   },
   photo: {
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  sum: {
+  summary: {
+    type: String,
+    required: true
+  },
+  note: {
     type: Schema.Types.ObjectId,
-    ref: "Summary"
+    ref: "Note"
+  },
+  saved: {
+    type: Boolean,
+    required: true,
+    default: false
   }
-
+ 
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var articles = mongoose.model("Articles", ArticleSchema);
+var Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
-module.exports = articles;
+module.exports = Article;
--- server.js
+++ server.js
@@ -1,44 +1,45 @@
 // Dependencies
-const express = require("express");
-// const mongojs = require("mongojs");
-const mongoose = require("mongoose");
-// Require axios and cheerio. This makes the scraping possible
-const axios = require("axios");
-const cheerio = require("cheerio");
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
 // Initialize Express
-const app = express();
+var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-const db = require("./models");
+var db = require("./models");
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newies";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/npr";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).then(function () {
+  console.log(`Connected to database ${MONGODB_URI}`);
+});
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-
-// Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/newies", { useNewUrlParser: true });
-
-// Database configuration
-// const databaseUrl = "newies";
-// const collections = ["newiesData"];
-
-// // Hook mongojs configuration to the db constiable
-// const db = mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
+// sConnect to the Mongo DB
+//  mongoose.connect("mongodb://localhost/npr", { useNewUrlParser: true });
 
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
+  db.Article.find({})
+  .then(function(dbArticle) {
+  
+      res.json(dbArticle);
+     // var artcl = {article: found}
+      //res.render(index,artcl)
+    
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    res.json(err);
+  });
+
   res.render("index");
 });
 
@@ -48,37 +49,40 @@ app.get("/", function(req, res) {
 
 app.get("/scrape", function(req, res) {
 axios.get("https://www.npr.org/").then(function(response) {
-  const $ = cheerio.load(response.data);
-  
-  $("div.story-wrap").each(function(i, element) {
-    const results = [];
-    const title = $(element).find("h3").text().trim();
-    const link = $(element).find("a").attr("href");
-    const photo = $(element).find("img.img").attr("src");
-    const sum = $(element).find("p.teaser").text();
-      console.log(photo);
-      console.log(sum);
-    if (title && link && photo && sum) {
-      if(results.indexOf(title) === -1){
-        results.push(title);
+  var $ = cheerio.load(response.data);
+ 
 
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link,
-          photo: photo,
-          summary: sum
-        },
-        function(err, inserted) {
-          if (err) {
-            console.log(err);
-          }
-          else {
-            console.log(inserted);
-          }
+  $("div.story-wrap").each(function(i, element) {
+    var results = {};
+    results.title = $(element)
+    .find("h3")
+    .text()
+    .trim(); 
+    results.link = $(element)
+    .find("a")
+    .attr("href");
+    results.photo = $(element)
+    .find("img.img")
+    .attr("src");
+    results.summary = $(element)
+    .find("p.teaser")
+    .text();
+      // console.log(photo);
+      console.log(results.summary);
+    if (results.title && results.link && results.photo && results.summary) {
+      if(results.title.indexOf(results.title) === -1){
+        results.push(results.title);
+       }
+       db.Article.create(results)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
         });
 
-      }
     }
   });
 });
@@ -87,19 +91,9 @@ axios.get("https://www.npr.org/").then(function(response) {
 res.send("Scrape Complete");
 });
 
-app.get("/all", function(req, res) {
+app.get("/article", function(req, res) {
 
-  db.scrapedData.find({})
-  .then(function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-     // const artcl = {article: found}
-      //res.render(index,artcl)
-    }
-  });
+  
 });
 // Listen on port 3000
 app.listen(PORT, function() {
